9db0c5b1332836cbcde4526ef94f982ac97a0cd5
MarinaMi12 <marina.mishchyshyna@gmail.com>
2024-05-02 12:28:40 +0200

    ukol

---
+++ Lekce 25.04/Lekce 25.04/palindrom1.cs
@@ -0,0 +1,34 @@
+﻿using System.Security.Cryptography.X509Certificates;
+using System.Text;
+
+namespace Lekce_25._04
+{
+    internal class palindrom1
+    {
+        static void Main(string[] args)
+        {
+            public class Program
+        {
+            public static void Main()
+            {
+
+                //Ukol - Obratte poradi stringu
+                string palindrom = "Kuna nese nanuk";
+
+                for (int i = 0; i < palindrom.Length; i++)
+                {
+                    Console.Write(palindrom[palindrom.Length - i]);
+                }
+
+
+               
+
+            }
+
+
+        } }
+}
+    
+    
+
+
---
+++ Lekce 25.04/Lekce 25.04/palindrom2.cs
@@ -0,0 +1,41 @@
+﻿using System.Text;
+
+namespace Lekce_25._04
+{
+    internal class palindrom1
+    {
+        static void Main(string[] args)
+        {
+            public class Program
+
+
+                //Ukol - Napiste funkci, ktera umi detekovat, ze se jedna o palindrom (zatim jen u slov) a pak z pole vypiste jen palindromy
+                string[] slova = new string[] { "kajak", "program", "rotor", "Czechitas", "madam", "Jarda", "radar", "nepotopen" };
+
+        //Ukol - opravte v tomto textu omylem zapnuty Caps Lock
+        //string capsLock = "jAK mICROSOFT wORD POZNA ZAPNUTY cAPSLOCK";
+
+
+        //Ukol - rozsifrujte tuto zpravu - text byl zasifrovan tak, ze jsme kazde pismeno posunuli o jedno doprava: 'a' -> 'b'. 
+        //string sifra = "Wzcpsob!qsbdf!.!hsbuvmvkj!b!ktfn!ob!Ufcf!qztoz";
+
+
+    
+    }
+    public static string otocSlovo(string slovo)
+    {
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < slovo.Length; i++)
+        {
+
+        }
+    }
+
+}
+
+
+
+}
+
+
